f188f5e09a2d6de5f913bef239104454f59eecdb
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-16 15:47:10 +0200

    finished first build

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { Toaster } from "src/components/ui/toaster";
+import { Toaster as Sonner } from "src/components/ui/sonner";
+import { TooltipProvider } from "src/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Dashboard from "./pages/Dashboard";
+import Leaderboard from "./pages/Leaderboard";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          } />
+          <Route path="/leaderboard" element={
+            <Layout>
+              <Leaderboard />
+            </Layout>
+          } />
+          <Route path="/profile" element={
+            <Layout>
+              <Profile />
+            </Layout>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
